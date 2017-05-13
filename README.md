c6b6a45fe8419f1c4c72658f39825b8c511b03ee
Mikael Puittinen <mikael@iki.fi>
2017-05-13 22:41:10 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,110 @@
+# Serverless KMS Secrets
+
+A Serverless Plugin for the [Serverless Framework](http://www.serverless.com) which
+helps with encrypting service secrets using the AWS Key Management Service (KMS)
+
+**THIS PLUGIN REQUIRES SERVERLESS V1.0 BETA OR NEWER!**
+
+## Introduction
+
+This plugins does the following:
+
+* It provides commands to encrypt and decrypt secrets with KMS
+
+## Installation and configuration
+
+In your service root, run:
+
+```bash
+npm install --save-dev serverless-kms-secrets
+```
+
+Add the plugin to `serverless.yml`:
+
+```yml
+plugins:
+  - serverless-kms-secrets
+```
+
+Configure the plugin into the custom block in `serverless.yml`. For example:
+
+```yml
+custom:
+  serverless-kms-secrets
+    keyId: [your hexadecimal KMS Key Id]
+  kmsSecrets: ${file(kms-secrets.${opt:stage, self:provider.stage}.${opt:region, self:provider.region}.yml)}
+```
+
+By default, the plugin creates secrets to the file kms-secrets.[stage].[region].yml. This can be overriden with the secretsFile parameter in the serverless-kms-secrets configuration.
+
+Add Decrypt permissions to your lambda function with e.g. this block in IamRoleStatements:
+
+```yml
+    - Effect: Allow
+      Action:
+      - KMS:Decrypt
+      Resource: ${self:custom.kmsSecrets.keyArn} 
+```
+
+## Usage
+
+### Encrypting Variables
+
+To encrypt a variable using the key defined in the configuration, enter
+```
+sls encrypt -n VARIABLE_NAME -v myvalue
+```
+
+e.g.
+
+```
+sls encrypt -n SLACK_API_TOKEN -v xoxp-1234567890-1234567890-123467890-a12346
+```
+
+The encrypted variable is written to your secrets file (kms-secrets.[stage].[region].yml by default)
+
+### Decrypting Variables
+
+The variables in the secrets file can be decrypted using
+
+```
+sls decrypt [-n VARIABLE_NAME]
+```
+
+The -n option is optional. Without that, all variables are decrypted and displayed in clear text on the console.
+
+### Using variables
+
+Pass the variables stored in the secrets file e.g. as environment variables using
+
+```yml
+  environment:
+    MY_VARIABLE: ${self:custom.kmsSecrets.secrets.MY_VARIABLE}
+```
+
+The variable must be decrypted in the Lambda function using the KMS decrypt method. E.g.
+
+```js
+kms.decrypt({
+  CiphertextBlob: Buffer(config[val], 'base64')
+}).promise()
+.then(data => {
+  const decrypted = String(data.Plaintext)
+})
+```
+
+
+## TODO
+
+* Add support for sls deploy (deploy as KMS encrypted environment variables)
+* Ease configuration (KeyIds / Arns in various places)
+
+## Release History
+
+* 2017/05/13 - v0.9.0 - Initial version
+
+
+## License
+
+Copyright (c) 2017 [SC5](http://sc5.io/), licensed for users and contributors under MIT license.
+https://github.com/SC5/serverless-kms-secrets/blob/master/LICENSE
---
+++ index.js
@@ -0,0 +1,204 @@
+'use strict';
+
+/**
+ * serverless-mocha-plugin
+ * - a plugin for TDD with Serverless Framework
+ */
+
+const path = require('path');
+const fse = require('fs-extra');
+const utils = require('./utils');
+const BbPromise = require('bluebird');
+const yaml= require('yamljs');
+const AWS = require('aws-sdk');
+AWS.config.setPromisesDependency(BbPromise);
+
+
+const validFunctionRuntimes = [
+  'aws-nodejs4.3',
+];
+
+const humanReadableFunctionRuntimes = `${validFunctionRuntimes
+  .map(template => `"${template}"`).join(', ')}`;
+
+// Update new value into the serverless key config file
+function updateFile(filePath, varName, encValue, keyArn) {
+  return new BbPromise((success, error) => {
+    let kmsSecrets = {
+      secrets: {},
+      keyArn: keyArn
+    };
+    if (fse.existsSync(filePath)) {
+      kmsSecrets = yaml.load(filePath);
+    }
+
+    kmsSecrets.secrets[varName] = encValue;
+    kmsSecrets.keyArn = keyArn;
+
+    fse.writeFileSync(filePath, yaml.stringify(kmsSecrets,2));
+    return success();
+  });
+}
+
+function readFile(filePath) {
+  return new BbPromise((success, error) => {
+    if (! fse.existsSync(filePath)) {
+      error(`No file ${filePath}`);
+    }
+
+    const fileContent = fse.readFileSync(filePath);
+    const kmsSecrets = yaml.load(filePath);
+
+    return success(kmsSecrets.secrets);
+  });
+}
+
+class kmsSecretsPlugin {
+  constructor(serverless, options) {
+    this.serverless = serverless;
+    this.options = options;
+
+    this.commands = {
+      encrypt: {
+        usage: 'Encrypt variables to file',
+        lifecycleEvents: [
+          'kmsEncrypt',
+        ],
+        options: {
+          name: {
+            usage: 'Name of variable',
+            shortcut: 'n',
+            required: true,
+          },
+          value: {
+            usage: 'Value of variable',
+            shortcut: 'v',
+            required: true,
+          },
+        },
+      },
+      decrypt: {
+        usage: 'Decrypt variables from file',
+        lifecycleEvents: [
+          'kmsDecrypt',
+        ],
+        options: {
+          name: {
+            usage: 'NName of variable',
+            shortcut: 'n',
+            required: false
+          },
+        },
+      },
+    };
+
+    this.hooks = {
+      'encrypt:kmsEncrypt': () => {
+        BbPromise.bind(this)
+          .then(this.encryptVariable);
+      },
+      'decrypt:kmsDecrypt': () => {
+        BbPromise.bind(this)
+          .then(this.decryptVariable);
+      },
+    };
+  }
+
+  encryptVariable() {
+    const myModule = this;
+    const stage = this.options.stage;
+    const region = this.options.region
+    
+    this.serverless.service.load({
+      stage,
+      region,
+    })
+    .then((inited) => {
+      myModule.serverless.environment = inited.environment;
+      const vars = new myModule.serverless.classes.Variables(myModule.serverless);
+      vars.populateService(this.options);
+
+      let moduleConfig = inited.custom['serverless-kms-secrets'];
+      if (! moduleConfig) {
+        myModule.serverless.cli.log('No configuration for serverless-kms-secrets in serverless.yml');
+        return;
+      }
+      if (! moduleConfig.keyId) {
+        myModule.serverless.cli.log('No keyId in serverless.yml');
+        return;
+      }
+
+      const configFile = moduleConfig.secretsFile || `kms-secrets.${inited.provider.stage}.${inited.provider.region}.yml`;
+
+      AWS.config.update({ region: inited.provider.region });
+      const kms = new AWS.KMS();
+      kms.encrypt({
+        KeyId: moduleConfig.keyId, // The identifier of the CMK to use for encryption. You can use the key ID or Amazon Resource Name (ARN) of the CMK, or the name or ARN of an alias that refers to the CMK.
+        Plaintext: Buffer(this.options.value)
+      }).promise()
+      .then(data => {
+
+        updateFile(configFile, this.options.name, data.CiphertextBlob.toString('base64'), data.KeyId)
+        .then(() => {
+          myModule.serverless.cli.log(`Updated ${this.options.name} to ${configFile}`);
+        }, error => {
+          myModule.serverless.cli.log(`Error updating ${configFile} : ${error}`);
+        })
+      }, error => {
+        myModule.serverless.cli.log(error );
+      })
+    }, error => myModule.serverless.cli.log(error));
+  }
+
+// Decrypt a variable defined in the options 
+  decryptVariable() {
+    const myModule = this;
+
+    const stage = this.options.stage;
+    const region = this.options.region;
+
+    this.serverless.service.load({
+      stage,
+      region,
+    })
+    .then((inited) => {
+      myModule.serverless.environment = inited.environment;
+      const vars = new myModule.serverless.classes.Variables(myModule.serverless);
+      vars.populateService(this.options);
+
+      let moduleConfig = inited.custom['serverless-kms-secrets'];
+      if (! moduleConfig) {
+        myModule.serverless.cli.log('No configuration for serverless-kms-secrets in serverless.yml');
+        return;
+      }
+
+      const configFile = moduleConfig.secretsFile || `kms-secrets.${inited.provider.stage}.${inited.provider.region}.yml`;
+      myModule.serverless.cli.log(`Decrypting secrets from ${configFile}`);
+
+      readFile(configFile)
+      .then(secrets => {
+        const vars = this.options.name? [ this.options.name ] : Object.keys(secrets);
+
+        vars.forEach((varName, idx) => {
+          if (secrets[varName]) {
+            AWS.config.update({ region: inited.provider.region });
+            const kms = new AWS.KMS();
+            kms.decrypt({
+              CiphertextBlob: Buffer(secrets[varName], 'base64')
+            }).promise()
+            .then(data => {
+              const secret = String(data.Plaintext);
+              myModule.serverless.cli.log(`${varName} = ${secret}`)
+            }, error => {
+              myModule.serverless.cli.log(`KMS error ${error}`);
+            })
+          } else {
+            myModule.serverless.cli.log(`No secret with name ${varName}`);
+          }
+        });
+      }, error => myModule.serverless.cli.log(error));
+    }, error => myModule.serverless.cli.log(error));
+  }
+}
+
+module.exports = kmsSecretsPlugin;
